4011c7b500ef996470d48ceb59c9503a5cd77111
Le Van Thien <20521947@gm.uit.edu.vn>
2024-07-09 17:21:56 +0700

    online users events, send&get message events

--- src/config/socket.js
+++ src/config/socket.js
@@ -4,21 +4,43 @@ import config from "./config.js";
 const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: config.web.origin,
+            origin: [config.web.origin],
             methods: ["GET", "POST"],
         },
     });
 
+    let onlineUsers = [];
+
     io.on("connection", (socket) => {
         console.log("a user connected");
 
-        socket.on("chat message", (msg) => {
-            console.log("message: " + msg);
-            io.emit("chat message", msg);
+        socket.on("add-online-users", (userId) => {
+            if (!onlineUsers.some((user) => user.user_id === userId))
+                onlineUsers.push({
+                    user_id: userId,
+                    socket_id: socket.id,
+                });
+            io.emit("send-online-users", onlineUsers);
+            console.log("added");
+        });
+
+        socket.on("send-message", (message, receiverIds) => {
+            receiverIds.forEach((receiverId) => {
+                const user = onlineUsers.find(
+                    (user) => user.user_id === receiverId
+                );
+                if (user) {
+                    io.to(user.socket_id).emit("get-message", message);
+                }
+            });
         });
 
         socket.on("disconnect", () => {
-            console.log("a user disconnected");
+            console.log("user disconnected");
+            onlineUsers = onlineUsers.filter(
+                (user) => user.socket_id !== socket.id
+            );
+            io.emit("send-online-users", onlineUsers);
         });
     });
 };
